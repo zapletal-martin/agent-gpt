1bede2ae3e785d22e47a1c99e2904dd924acb1fa
Martin Zapletal <zapletal-martin@email.cz>
2024-08-04 15:52:36 -0400

    Teacher student LLM interaction.

--- langchain/agent_math.py
+++ langchain/agent_math.py
@@ -1,7 +1,32 @@
 # Set env var OPENAI_API_KEY or load from a .env file:
 import dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 dotenv.load_dotenv()
 
-chat = ChatOpenAI(model="gpt-4o-mini", temperature=0.2)
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=1)
+
+teacher_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are math teacher. You job is to ask simple math questions. Be creative. Only respond with the single question."),
+    ("user", "{input}")
+])
+
+teacher_output_parser = StrOutputParser()
+
+teacher = teacher_prompt | llm
+
+question = teacher.invoke({"input": "Can you please ask me a math question?"})
+print(question.content)
+
+student_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are math student. Your task is to answer math questions."),
+    ("user", "{input}")
+])
+
+student_output_parser = StrOutputParser()
+
+student = student_prompt | llm | student_output_parser
+
+print(student.invoke({"input": question.content}))
--- langchain/website.py
+++
@@ -1,7 +0,0 @@
-# Set env var OPENAI_API_KEY or load from a .env file:
-import dotenv
-from langchain_openai import ChatOpenAI
-
-dotenv.load_dotenv()
-
-chat = ChatOpenAI(model="gpt-4o-mini", temperature=0.2)
